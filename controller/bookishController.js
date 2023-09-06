cdfcfd767d483da59a1721ea0b6e9c0da11f489d
czeska120 <franczeska_silvestre@dlsu.edu.ph>
2023-09-06 19:10:00 +0800

    Integrating New York Times Book API

--- controller/bookishController.js
+++ controller/bookishController.js
@@ -1,7 +1,27 @@
+const axios = require("axios");
+
 const bookishController = {
     // for redirecting login and signup
     getLanding: async function(req, res) {
-       res.render('index');
+        const apiKey = process.env.BOOKS_AUTH_KEY;
+        console.log(apiKey);
+        const list = "hardcover-fiction"
+        const url = 'https://api.nytimes.com/svc/books/v3/lists/current/'+list+'.json?api-key='+apiKey;
+        var bookListData = "";
+        var bookData = ""
+
+        try {
+            const response = await axios.get(url);
+            bookListData = response.data;
+            bookData = response.data.results.books;
+        } catch (err) {
+            console.log(err);
+        }
+        console.log(bookListData);
+        console.log("============== ONE BOOK AUTHOR ==========")
+        console.log(bookData[0].author);
+        res.render('index', {bookListData, bookData});
     }
+    
 }
 module.exports = bookishController;
--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,64 @@
 window.onscroll = function() {scrollFunction()};
     
 function scrollFunction() {
+    const allArticles = document.querySelectorAll("article");
+
     if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
-    document.getElementById("navbar").style.top = "0";
-    document.getElementById("search").classList.add("visible");
+        document.getElementById("navbar").style.top = "0";
+        document.getElementById("search").classList.add("visible");
+        for (let article of allArticles) {
+            article.classList.add("visible");
+            article.classList.add("visible");
+        }
     } else {
-    document.getElementById("navbar").style.top = "-150px";
-    document.getElementById("search").classList.remove("visible");
+        document.getElementById("navbar").style.top = "-150px";
+        document.getElementById("search").classList.remove("visible");
+        for (let article of allArticles) {
+            article.classList.remove("visible");
+            article.classList.remove("visible");
+        }
     }
 }
+
+// CAROUSEL JS
+const scrollContainers = document.querySelectorAll("#infiniteScroll--left");
+
+scrollContainers.forEach((container) => {
+	const scrollWidth = container.scrollWidth;
+	let isScrollingPaused = false;
+
+	window.addEventListener("load", () => {
+		self.setInterval(() => {
+			if (isScrollingPaused) {
+				return;
+			}
+			const first = container.querySelector("article");
+
+			if (!isElementInViewport(first)) {
+				container.appendChild(first);
+				container.scrollTo(container.scrollLeft - first.offsetWidth, 0);
+			}
+			if (container.scrollLeft !== scrollWidth) {
+				container.scrollTo(container.scrollLeft + 1, 0);
+			}
+		}, 15);
+	});
+
+	function isElementInViewport(el) {
+		var rect = el.getBoundingClientRect();
+		return rect.right > 0;
+	}
+
+	function pauseScrolling() {
+		isScrollingPaused = true;
+	}
+
+	function resumeScrolling() {
+		isScrollingPaused = false;
+	}
+	const allArticles = container.querySelectorAll("article");
+	for (let article of allArticles) {
+		article.addEventListener("mouseenter", pauseScrolling);
+		article.addEventListener("mouseleave", resumeScrolling);
+	}
+});
